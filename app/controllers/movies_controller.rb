d7d45e252f0314ad7defa123466f873d9db96682
chloeattlan <164663007+chloeattlan@users.noreply.github.com>
2025-02-19 23:34:07 +0000

    made very little progress

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -14,4 +14,22 @@ class MoviesController < ApplicationController
 
     render({ :template => "movie_templates/show" })
   end
+
+  def new_movie
+    @title = params["title"]
+    @year = params["year"]
+    @description = params["description"]
+    @director_id = params["director_id"]
+    @image = params["image"]
+
+    new_movie = Movie.new
+    new_movie.title = @title
+    new_movie.year = @year
+    new_movie.description = @description
+    new_movie.director_id = @director_id
+    new_movie.image = @image
+    new_movie.save
+    redirect_to("/movie_templates/index")
+
+  end
 end
