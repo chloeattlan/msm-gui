88e053dce7fca28c6394d05e3841515b5f51167c
chloeattlan <164663007+chloeattlan@users.noreply.github.com>
2025-02-24 04:02:50 +0000

    finished directors

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -36,4 +36,37 @@ class DirectorsController < ApplicationController
 
     render({ :template => "director_templates/eldest" })
   end
+
+  def insert_director
+    director = Director.new
+    director.name = params['name']
+    director.dob = params['dob']
+    director.bio = params['bio']
+    director.image = params['image']
+
+    director.save
+
+    redirect_to("/directors")
+  end
+
+  def delete_director
+    the_id = params['director_id']
+    director = Director.where({ :id => the_id})[0]
+    director.destroy
+    redirect_to("/directors")
+  end
+
+  def modify_director
+    the_id = params['director_id']
+    director = Director.where({ :id => the_id})[0]
+
+    director.name = params['name']
+    director.dob = params['dob']
+    director.bio = params['bio']
+    director.image = params['image']
+
+    director.save
+
+    redirect_to("/directors/#{director.id}")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
 
   get("/directors", { :controller => "directors", :action => "index" })
   get("/directors/:path_id", { :controller => "directors", :action => "show" })
+  post("/insert_director", { :controller => "directors", :action => "insert_director" })
+  get("/delete_director/:director_id", { :controller => "directors", :action => "delete_director" })
+  post("/modify_director/:director_id", { :controller => "directors", :action => "modify_director"})
 
   get("/movies", { :controller => "movies", :action => "index" })
   get("/movies/:path_id", { :controller => "movies", :action => "show" })
