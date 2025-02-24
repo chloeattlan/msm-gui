90081253d8bf408e08945f52a5367d498208adf2
chloeattlan <164663007+chloeattlan@users.noreply.github.com>
2025-02-24 04:40:38 +0000

    done

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -14,4 +14,35 @@ class ActorsController < ApplicationController
       
     render({ :template => "actor_templates/show" })
   end
+
+  def insert_actor
+    actor = Actor.new
+    actor.name = params['name']
+    actor.dob = params['dob']
+    actor.bio = params['bio']
+    actor.image = params['image']
+    actor.save
+
+    redirect_to("/actors")
+  end
+
+  def delete_actor
+    the_id = params['actor_id']
+    act = Actor.where({ :id => the_id})[0]
+    act.destroy
+    redirect_to("/actors")
+  end
+
+  def modify_actor
+    the_id = params['actor_id']
+    actor = Actor.where({ :id => the_id})[0]
+    
+    actor.name = params['name']
+    actor.dob = params['dob']
+    actor.bio = params['bio']
+    actor.image = params['image']
+    actor.save
+
+    redirect_to("/actors/#{actor.id}")
+  end
 end
